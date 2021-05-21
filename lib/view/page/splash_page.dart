791ec4f110b3513347eea41c42e2039b43459e03
cmdmxdz <xdz_coder@163.com>
2021-05-21 15:45:28 +0800

    【新增】增加了启动页倒计时3秒跳转的逻辑（后续可动态修改倒计时时间）。 [Added] Added the logic of the 3 second countdown jump on the startup page (the countdown time can be dynamically modified later).

--- lib/view/page/splash_page.dart
+++ lib/view/page/splash_page.dart
@@ -1,26 +1,66 @@
+import 'dart:async';
 
+import 'package:fluro/fluro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lazy_account/view/router/router_helper.dart';
 
+/// 启动页
 class SplashPage extends StatefulWidget {
-  SplashPage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  /// 倒计时时间
+  static const COUNT_DOWN_TIME = 3;
 
   @override
   _SplashPageState createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
+  Timer _timer;
+  int count = SplashPage.COUNT_DOWN_TIME;
+
+  /// https://www.cnblogs.com/sangwl/p/11435191.html
+  /// 广告页倒计时
+  _startTime() async {
+    //设置启动图生效时间
+    var _duration = new Duration(seconds: 1);
+    new Timer(_duration, () {
+      // 空等1秒之后再计时
+      _timer = new Timer.periodic(const Duration(milliseconds: 1000), (v) {
+        count--;
+        if (count == 0) {
+          _navigationPage();
+        } else {
+          setState(() {});
+        }
+      });
+      return _timer;
+    });
+  }
+
+  /// 跳转页面
+  void _navigationPage() {
+    _timer.cancel();
+    /// 要跳转的页面
+    RouterHelper.router.navigateTo(context, "LoginPage",
+        transition: TransitionType.inFromRight);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startTime();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    /// 突然关闭时，若倒计时未停止则手动停止
+    if (_timer.isActive) {
+      _timer.cancel();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,14 +68,9 @@ class _SplashPageState extends State<SplashPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-                '欢迎使用懒人记账应用',
-                style: Theme.of(context).textTheme.headline6
-            ),
-            Text(
-              'Welcome to use App LazyAccount',
-                style: Theme.of(context).textTheme.headline6
-            ),
+            Text('欢迎使用懒人记账应用', style: Theme.of(context).textTheme.headline6),
+            Text('Welcome to use App LazyAccount',
+                style: Theme.of(context).textTheme.headline6),
           ],
         ),
       ),
