ea3da91c4e39864fc204c7515d45773ddca556d3
cmdmxdz <xdz_coder@163.com>
2021-05-31 00:37:07 +0800

    【新增】添加了动态表格样式模块设置项，待完善。 [Added] Added dynamic table style module setting item to be improved.

--- lib/view/page/personal_page.dart
+++ lib/view/page/personal_page.dart
@@ -30,10 +30,27 @@ class _PersonalPageState extends State<PersonalPage> {
     super.dispose();
   }
 
+  void _initData() {
+    if (cardList.isNotEmpty) {
+      cardList.clear();
+    }
+    cardList.add(
+        UserInfo(0, 0, "images/default_avatar.png", "name1", "infoContent1"));
+    cardList.add(
+        UserInfo(0, 1, "images/default_avatar.png", "name2", "infoContent2"));
+    cardList.add(
+        UserInfo(1, 0, "images/default_avatar.png", "name3", "infoContent3"));
+    cardList.add(
+        UserInfo(1, 1, "images/default_avatar.png", "name4", "infoContent4"));
+    cardList.add(
+        UserInfo(2, 0, "images/default_avatar.png", "name5", "infoContent5"));
+  }
+
   @override
   Widget build(BuildContext context) {
+    _initData();
     return Scaffold(
-      body: Column(
+      body: ListView(
         children: [
           Stack(
             children: [
@@ -42,7 +59,8 @@ class _PersonalPageState extends State<PersonalPage> {
                 color: Colors.orangeAccent,
               ),
               Container(
-                margin: EdgeInsets.only(top: 50, left: 50, right: 50),
+                margin:
+                    EdgeInsets.only(top: 50, left: 50, right: 50, bottom: 30),
                 color: Colors.transparent,
                 child: Card(
                   child: Row(
@@ -61,25 +79,26 @@ class _PersonalPageState extends State<PersonalPage> {
             ],
           ),
           Container(
-            height: 50,
-            color: Colors.red,
+            // margin: EdgeInsets.only(
+            //   left: 20,
+            //   right: 20,
+            // ),
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 15,
+                    childAspectRatio: 1),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return _getCardWidget(index, cardList[index]);
+                },
+                itemCount: cardList?.length ?? 0),
           ),
-//          ListView.separated(
-//              shrinkWrap: true,
-//              physics: NeverScrollableScrollPhysics(),
-//              itemBuilder: (BuildContext context, int index) {
-//                return _getDaySubBoxWidget(
-//                    context, index, activitiesList[index]);
-//              },
-//              separatorBuilder: (context, index) => Container(
-////                height: ScreenUtil.getInstance().setWidth(56),
-//              ),
-//              itemCount: activitiesList?.length ?? 0),
-          cardWidget,
           Container(
             height: 30,
-            color: Colors.blue,
-          )
+          ),
         ],
       ),
     );
@@ -159,4 +178,88 @@ class _PersonalPageState extends State<PersonalPage> {
       ),
     ),
   );
+
+  Widget _getCardWidget(int index, UserInfo userInfo) {
+    return _getShadowWidget(
+        null,
+        null,
+        index == cardList.length - 1 ? 30.0 : 0.0,
+        userInfo.y == 0 ? 30.0 : 0.0,
+        userInfo.y == 1 ? 30.0 : 0.0,
+        Container(
+          // color: Colors.yellow[600],
+          padding: EdgeInsets.only(top: 15.0, left: 15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.yellow[800], width: 1),
+                    borderRadius: BorderRadius.circular(42)),
+                child: ClipOval(
+                  child: Image.asset(
+                    null == userInfo.iconPath ? "" : userInfo.iconPath,
+                    width: 83,
+                    height: 83,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  top: 15.0,
+                ),
+                child: Text(
+                  null == userInfo.name ? "" : userInfo.name,
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  top: 15.0,
+                ),
+                child: Text(
+                  null == userInfo.infoContent ? "" : userInfo.infoContent,
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  /// 方框阴影
+  Widget _getShadowWidget(num height, num marginTop, num marginBottom,
+      num marginLeft, num marginRight, Widget subWidget) {
+    return Container(
+      height: null == height ? null : height,
+      margin: EdgeInsets.only(
+        top: null == marginTop ? 0 : marginTop,
+        left: null == marginLeft ? 0 : marginLeft,
+        right: null == marginRight ? 0 : marginRight,
+        bottom: null == marginBottom ? 0 : marginBottom,
+      ),
+      decoration: BoxDecoration(
+          color: Color(0xCCFFFFFF),
+          borderRadius: BorderRadius.all(Radius.circular(24)),
+          boxShadow: [
+            //refer to :https://ninghao.net/video/6443
+            BoxShadow(
+                color: Color(0xFFDBDEE3),
+                offset: Offset(12, 12),
+                blurRadius: 20,
+                spreadRadius: 0.0),
+          ]),
+      child: subWidget,
+    );
+  }
 }
